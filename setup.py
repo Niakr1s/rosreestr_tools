f9be17fce900850a13136fb408c6354c9d3cd288
Niakr1s <pavel2188@gmail.com>
2018-04-06 14:10:22 +0300

    added setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from cx_Freeze import setup, Executable
 
-executables = [Executable('rosreestr_tools.py')]
+executables = [
+    Executable('rosreestr_tools.py', shortcutDir='DesktopFolder', shortcutName='Rosreestr Tools', icon='static/rt.ico')]
 
 setup(name='rosreestr_tools', version='0.1.1', description='Rosreestr Tools', executables=executables,
-      package_dir={'': 'scripts'}, )
+      package_dir={'': 'scripts'})
