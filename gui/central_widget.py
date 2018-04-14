4b59be9191deccde097a8566a068290d509cb8ca
Niakr1s <pavel2188@gmail.com>
2018-04-14 17:51:58 +0300

    added menu, status, tool to main_window, added button_clicked event to central_widget lists

--- gui/central_widget.py
+++ gui/central_widget.py
@@ -1,3 +1,5 @@
+import logging
+
 from PyQt5 import QtWidgets
 
 
@@ -12,40 +14,71 @@ class CentralWidget(QtWidgets.QWidget):
         self.dxf_view = DxfListView(self)
 
         # Right column
-        self.output_view = QtWidgets.QTextEdit(self)
+        self.output_view = OutputView(self)
 
         # Creating main layout
-        box = QtWidgets.QHBoxLayout(self)
-        box.addWidget(self.xml_view)  # Left column
-        box.addWidget(self.dxf_view)  # Middle column
-        box.addWidget(self.output_view)  # Right column
+        main_layout = QtWidgets.QHBoxLayout(self)
+        main_layout.addWidget(self.xml_view)  # Left column
+        main_layout.addWidget(self.dxf_view)  # Middle column
+        main_layout.addWidget(self.output_view)  # Right column
 
-        self.setLayout(box)
+        self.setLayout(main_layout)
 
 
 class MyListView(QtWidgets.QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, file_type, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
+        self.file_type = file_type
 
         self.list_view = QtWidgets.QListView(self)
+
         self.btn_add = QtWidgets.QPushButton('add')
+        self.btn_add.clicked.connect(self.on_btn_add_click)
+
         self.btn_delete = QtWidgets.QPushButton('delete')
 
-        vbox = QtWidgets.QVBoxLayout(self)
-        vbox.addWidget(self.list_view)
+        # Top layout with buttons
+        top_layout = QtWidgets.QHBoxLayout()
+        top_layout.addWidget(self.btn_add)
+        top_layout.addWidget(self.btn_delete)
 
-        hbox = QtWidgets.QHBoxLayout(self)
-        hbox.addWidget(self.btn_add)
-        hbox.addWidget(self.btn_delete)
+        # Middle layout with view
+        mid_layout = QtWidgets.QHBoxLayout()
+        mid_layout.addWidget(self.list_view)
 
-        vbox.addLayout(hbox)
+        # Bottom layout with buttons
+        bot_layout = QtWidgets.QHBoxLayout()
+
+        # Main layout
+        main_layout = QtWidgets.QVBoxLayout()
+        main_layout.addLayout(top_layout)
+        main_layout.addLayout(mid_layout)
+        main_layout.addLayout(bot_layout)
+
+        self.setLayout(main_layout)
+
+    def on_btn_add_click(self):
+        # Making filter_string for QFileDialog
+        if self.file_type == 'xml':
+            filter_string = 'Выписки (*.%s)' % self.file_type
+        else:
+            filter_string = 'Чертеж (*.%s)' % self.file_type
+
+        file_names = QtWidgets.QFileDialog(self).getOpenFileNames(self, 'Добавить файлы', '', filter_string)
+        
+        logging.info('selected files: %s' % str(file_names[0]))
 
 
 class XmlListView(MyListView):
     def __init__(self, parent=None):
-        MyListView.__init__(self, parent)
+        MyListView.__init__(self, 'xml', parent)
 
 
 class DxfListView(MyListView):
     def __init__(self, parent=None):
-        MyListView.__init__(self, parent)
+        MyListView.__init__(self, 'dxf', parent)
+
+
+class OutputView(QtWidgets.QTextEdit):
+    def __init__(self, parent=None):
+        QtWidgets.QTextEdit.__init__(self, parent)
--- gui/main_window.py
+++ gui/main_window.py
@@ -1,3 +1,5 @@
+import logging
+
 from PyQt5 import QtWidgets
 
 from gui.central_widget import CentralWidget
@@ -8,12 +10,42 @@ class MainWindow(QtWidgets.QMainWindow):
         QtWidgets.QMainWindow.__init__(self)
         self.resize(1000, 600)
 
-        self.setCentralWidget(CentralWidget())
+        self.setCentralWidget(CentralWidget(self))
+        self.setStatusBar(StatusBar(self))
+        self.setMenuBar(MenuBar(self))
+        self.addToolBar(ToolBar(self))
 
+
+class StatusBar(QtWidgets.QStatusBar):
+    def __init__(self, parent=None):
+        QtWidgets.QStatusBar.__init__(self, parent)
+        self.showMessage('Добро пожаловать.')
+
+
+class MenuBar(QtWidgets.QMenuBar):
+    def __init__(self, parent=None):
+        QtWidgets.QMenuBar.__init__(self, parent)
+
+        file_menu = QtWidgets.QMenu('&Файл', self)
+        file_menu.addAction('&Выход')
+        self.addMenu(file_menu)
+
+        help_menu = QtWidgets.QMenu('&Справка', self)
+        help_menu.addAction('&Помощь')
+        help_menu.addAction('&О программе')
+        self.addMenu(help_menu)
+
+
+class ToolBar(QtWidgets.QToolBar):
+    def __init__(self, parent=None):
+        QtWidgets.QToolBar.__init__(self, parent)
+
+        self.addAction('Очистить')
 
 if __name__ == '__main__':
     import sys
 
+    logging.basicConfig(level=logging.INFO)
     app = QtWidgets.QApplication(sys.argv)
     main_window = MainWindow()
     main_window.move(-1200, 200)
