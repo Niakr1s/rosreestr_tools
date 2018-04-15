0428b8594ba87994106b4a8e9c176e46e5662c85
Niakr1s <pavel2188@gmail.com>
2018-04-15 19:04:31 +0300

    some 2exe fixes

--- rosreestr_tools_GUI.py
+++ rosreestr_tools_GUI.py
@@ -9,8 +9,9 @@ from gui.main_window import MainWindow
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     app = QtWidgets.QApplication(sys.argv)
+    app.setStyle('windowsvista')
     main_window = MainWindow()
-    main_window.move(-1200, 200)
+    # main_window.move(-1200, 200)
     main_window.show()
 
     if QtCore.QT_VERSION >= 0x50501:
--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
+import sys
+
 from cx_Freeze import setup, Executable
 
-executables = [
-    Executable('rosreestr_tools.py', shortcutDir='DesktopFolder', shortcutName='Rosreestr Tools', icon='static/rt.ico')]
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
 
-setup(name='rosreestr_tools', version='0.1.1', description='Rosreestr Tools', executables=executables,
-      package_dir={'': 'scripts'})
+executables = [Executable('rosreestr_tools_GUI.py', shortcutDir='DesktopFolder', shortcutName='Rosreestr Tools GUI',
+                          icon='static/rt.ico'),
+               Executable('rosreestr_tools.py', shortcutDir='DesktopFolder', shortcutName='Rosreestr Tools',
+                          icon='static/rt.ico'), ]
+
+setup(name='rosreestr_tools', version='1.0.0', description='Rosreestr Tools', executables=executables,
+      packages=['gui', 'scripts'], package_dir={'scripts': 'scripts', 'gui': 'gui'}, )
