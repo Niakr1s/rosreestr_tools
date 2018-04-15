f44769e2fb070a2b678c5caef5483ea330e6e8b1
Niakr1s <pavel2188@gmail.com>
2018-04-15 15:22:11 +0300

    added exception hook

--- rosreestr_tools_GUI.py
+++ rosreestr_tools_GUI.py
@@ -1,7 +1,8 @@
 import logging
 import sys
+import traceback
 
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 
 from gui.main_window import MainWindow
 
@@ -11,4 +12,11 @@ if __name__ == '__main__':
     main_window = MainWindow()
     main_window.move(-1200, 200)
     main_window.show()
+
+    if QtCore.QT_VERSION >= 0x50501:
+        def excepthook(type_, value, traceback_):
+            traceback.print_exception(type_, value, traceback_)
+            QtCore.qFatal('')
+    sys.excepthook = excepthook
+
     sys.exit(app.exec())
