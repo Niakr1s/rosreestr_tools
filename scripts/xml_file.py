14481e09abe8cbd8c3d57c03d58df398a9b3587d
Niakr1s <pavel2188@gmail.com>
2018-04-28 19:44:48 +0300

    added 'rect' option to parcels dict for each parcel

--- scripts/xml_file.py
+++ scripts/xml_file.py
@@ -12,6 +12,7 @@ from scripts.settings import Settings
 
 class XmlFile:
     """ Class for a single rosreestr xml file """
+
     def __init__(self, file_path):
         self.file_path = os.path.abspath(file_path)
         self.basename_file_path = os.path.basename(file_path)
@@ -19,9 +20,11 @@ class XmlFile:
         self.root = self.tree.getroot()
         # can be KPT, KVZU, KVOKS (Flat), KPOKS (Building, Construction, Uncompleted)
         self.xml_type = remove_namespace(self.root.tag)
-        self.cadastral_number = self.root.find('.//*//*[@CadastralNumber]').attrib['CadastralNumber']
-        self.parcels = self.get_parcels()  # sml_subtype should update after this
-        self.xml_subtype = remove_namespace(self.root.find('.//*[@CadastralNumber]').tag)
+        self.cadastral_number = self.root.find(
+            './/*//*[@CadastralNumber]').attrib['CadastralNumber']
+        self.parcels = self.get_parcels()
+        self.xml_subtype = remove_namespace(
+            self.root.find('.//*[@CadastralNumber]').tag)
         self.check = set()  # if myxml file checked and intersect parcel
 
     def get_parcels(self):
@@ -30,15 +33,40 @@ class XmlFile:
             cadastral_number = item.attrib['CadastralNumber']
             try:
                 if len(cadastral_number.split(':')) == 3:  # if it is a block
-                    actions.update(result, {cadastral_number: {'coordinates': self.get_block_conturs(item),
-                                                               'type': remove_namespace(item.tag)}})
+                    conturs = self.get_block_conturs(item)
+                    actions.update(result, {cadastral_number: {'coordinates': conturs,
+                                                               'type': remove_namespace(item.tag),
+                                                               'rect': self.get_rect(conturs)
+                                                               }})
                 else:
-                    actions.update(result, {cadastral_number: {'coordinates': self.get_parcel_conturs(item),
-                                                               'type': remove_namespace(item.tag)}})
+                    conturs = self.get_parcel_conturs(item)
+                    actions.update(result, {cadastral_number: {'coordinates': conturs,
+                                                               'type': remove_namespace(item.tag),
+                                                               'rect': self.get_rect(conturs)
+                                                               }})
             except NoCoordinates:
                 pass
                 # result[cadastral_number]['coordinates'] = self.get_parcel_conturs(item)
         # Removing blank keys
+        return result
+
+    @staticmethod
+    def get_rect(conturs):
+        # gets rectangle conturs as dict {'xmin', 'ymin', 'xmax', 'ymax'}
+        xs = []
+        ys = []
+        for contur in conturs:
+            for point in contur:
+                print(point)
+                xs.append(point[0])
+                ys.append(point[1])
+        result = {
+            'xmin': min(xs),
+            'xmax': max(xs),
+            'ymin': min(ys),
+            'ymax': max(ys),
+        }
+        print('get_rect function, result: ', result)
         return result
 
     @staticmethod
@@ -69,17 +97,21 @@ class XmlFile:
         if not self.parcels:
             return None
         dxffile = DxfFile(self)
-        logging.info('converting %s to %s' % (self.file_path, dxffile.output_file_path))
+        logging.info('converting %s to %s' %
+                     (self.file_path, dxffile.output_file_path))
         dxffile.draw_conturs_and_save()
         return dxffile.output_file_path
 
     @log
     def pretty_rename(self):
         dirpath = os.path.dirname(self.file_path)
-        cadastral_number_spaced = self.cadastral_number.replace(':', ' ') + '.xml'
-        pretty_basename = ' '.join((self.xml_type, self.xml_subtype, cadastral_number_spaced))
+        cadastral_number_spaced = self.cadastral_number.replace(
+            ':', ' ') + '.xml'
+        pretty_basename = ' '.join(
+            (self.xml_type, self.xml_subtype, cadastral_number_spaced))
         pretty_fullname = os.path.join(dirpath, pretty_basename)
-        logging.info('renaming from %s to %s' % (self.file_path, pretty_fullname))
+        logging.info('renaming from %s to %s' %
+                     (self.file_path, pretty_fullname))
         try:
             os.rename(self.file_path, pretty_fullname)
         except FileExistsError:
---
+++ tests/test_xml_file.py
@@ -0,0 +1,26 @@
+import filecmp
+import os
+import unittest
+
+from scripts.xml_file import XmlFile
+
+TEST = 'tests\\test'
+
+
+class Test_XmlFile(unittest.TestCase):
+    def test_get_rect(self):
+
+        xml = XmlFile('tests\\test\\xml\\KPT CadastralBlock 21 02 010106.xml')
+        print(xml.parcels['21:02:010106'])
+        etalon = {
+            'xmax': 408584.98,
+            'xmin': 407769.54,
+            'ymax': 1248481.90,
+            'ymin': 1246008.09,
+        }
+        self.assertDictEqual(
+            etalon, xml.parcels['21:02:010106']['rect'])  # testing
+
+
+if __name__ == '__main__':
+    unittest.main()
